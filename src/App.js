ec561606e89bd845bf33103bb9a5d815400e52af
elmehdi rhindi <g6handi@gmail.com>
2022-03-17 20:10:10 +0100

    routing

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import "./App.css";
 import { Collipsible } from "./components/Collipsible";
+import { Route, Router, Routes } from "react-router-dom";
 import { Section } from "./components/Section";
+import { Home } from "./routes/Home";
+import { Posts } from "./routes/Posts";
 
 function App() {
   return (
     <div className="app">
-      <Collipsible />
+      <h1>welcome</h1>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/posts" element={<Posts />} />
+      </Routes>
     </div>
   );
 }
